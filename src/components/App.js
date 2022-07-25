d4a91c873a6ba4a74c5c559b4c1faa92fae9d629
Ross McNabb <rossmcnabb@ymail.com>
2022-07-25 21:46:31 +0100

    Intial set up

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 
-import '../styles/App.css'
+import '../styles/App.css';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <div className="App">
  <h2>Surreal Estate</h2>
+ <NavBar/>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import '../styles/navbar.css'
+
+const NavBar = () =>{
+    return (
+        <div className="navBar">
+
+        </div>
+    )
+};
+export default NavBar;
