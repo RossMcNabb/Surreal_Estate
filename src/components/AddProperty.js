a61c18a43a2c30448584c8b589b0a0a3feae6a84
Ross McNabb <rossmcnabb@ymail.com>
2022-08-02 20:17:19 +0100

    Form creation including option fields

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -1,8 +1,50 @@
 import React from "react";
+import "../styles/add-property.css";
+import { useState } from "react";
 
 const AddProperty = () =>{
+    const intialState = {
+        title:"",
+        city:"Manchester"
+    }
+    const [fields, setFields] = useState(intialState.fields)
+    const handleAddProperty = (event) =>{
+        event.preventDefault();
+        console.log(fields);
+
+    }
+
+        const handleFieldChange = (event) => {
+            const changedField = event.target.name;
+            const newValue = event.target.value;
+            setFields({ ...fields, [changedField]: newValue });
+          };
+
+
     return(
-<div> AddProperty Page</div>
+<div className="add-property"> 
+        <form onSubmit={handleAddProperty}>
+        <label htmlFor="title">  </label>
+            <input id="title" name="title" value={fields.title} onChange={handleFieldChange}>
+            </input>
+          
+            <button type= "submit" >Add</button>
+            
+                <label html="city"></label>
+                <select id="city" name="city" value={fields.city} onChange={handleFieldChange}>
+                    <option value="Manchester"> Manchester</option>
+                    <option value="Leeds"> Leeds</option>
+                    <option value="Sheffield"> Sheffield</option>
+                    <option value="Liverpool"> Liverpool</option>
+                </select>     
+                
+         
+
+        </form>
+
+
+
+</div>
     );
 };
 
